74faa3df97734df690a4e0d15b280e6d49fa676c
ddvk <dobrinvk@outlook.com>
2019-04-08 20:56:17 +0200

    remove the binary from source control (output to bin folder which is ignored)

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-TARGET=touchinjector
+TARGET=bin/touchinjector
 
-$(TARGET): main.c
+$(TARGET): main.c 
+	@mkdir -p $(@D)
 	$(CC) -std=c99 -lm -o $@ main.c
 clean:
 	rm $(TARGET)
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-make
-scp touchinjector root@192.168.0.232:~/
-# ssh root@192.168.0.232 "~/touchinjector"
+#!/bin/sh
+make && scp bin/touchinjector root@remarkable:~/
+# ssh root@remarkable "~/touchinjector"
