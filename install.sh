912a31d52f9518aeb3f32d8102c1dc99da683e88
ddvk <36803246+ddvk@users.noreply.github.com>
2019-11-15 14:36:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #to be run on the device
-wget https://github.com/ddvk/remarkable-touchgestures/releases/download/0.0.3/touch.tgz -O - | tar xvf -
+wget https://github.com/ddvk/remarkable-touchgestures/releases/download/0.0.4/touch.tgz -O - | tar xvf -
 cp /home/root/touch.service /etc/systemd/system
 systemctl enable touch
 systemctl start touch
